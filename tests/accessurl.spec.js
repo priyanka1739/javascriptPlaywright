8142db91648873044b86502d3244e07747953038
priyanka <pkayast1@asu.edu>
2025-03-03 09:03:15 -0700

    practise flow commit

--- tests/accessurl.spec.js
+++ tests/accessurl.spec.js
@@ -3,11 +3,12 @@ import {test, expect} from '@playwright/test';
 
 test('access', async({page})=>{
  
-    
-    await page.getByRole('button', {name:"Log In to Sandbox"}).click();
-    await page.locator("[title ='Service Console']").waitFor();
-    await page.locator("[title ='Reports']").click();
+    // https://www.saucedemo.com/
+    await page.goto("https://www.saucedemo.com/");
+   await page.locator("#user-name").fill("standard_user");
+   await page.locator("#password").fill("secret_sauce");
     await page.pause();
+   await page.locator("#login-button").click();
 
 
 });
